0607d9777ff8381a3d9e8b1123926e7c74baa674
dreiver1 <viniciosdavid1@gmail.com>
2023-04-08 17:54:18 -0300

    refatoando metodos de recuperar estado e salvar historia

--- src/App.java
+++ src/App.java
@@ -1,49 +1,10 @@
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.stream.JsonReader;
-
 public class App {
 
-    public static JsonArray recuperaCapitulos(){
-        try {
-            File capitulos = new File("src/assets/capitulos.json");
-            FileReader fileReader = new FileReader(capitulos);
-            JsonReader jsonReader = new JsonReader(fileReader);
-            Gson gson = new Gson();
-            JsonArray array = gson.fromJson(jsonReader, JsonArray.class);
-            jsonReader.close();
-            fileReader.close();
-            return array;
-        } catch (IOException e) {
-            JsonArray array = new JsonArray();
-            return array;
-        }
-    }
-
-    public static void salvarHistoria(Historia m){
-        try {
-            File arquivo = new File("/home/david/Documentos/workspace/historia-interativa-arvore-decisao/src/assets/historia.json");
-            FileWriter fileWriter = new FileWriter(arquivo);
-            Gson gson = new Gson();
-            gson.toJson(m, fileWriter);
-            fileWriter.close();
-            return ;
-        } catch (IOException e) {
-            System.out.println(e);
-        }
-    }
-
     public static void main(String[] args) throws Exception {
-        JsonArray capitulos = recuperaCapitulos();
         Historia historia = new Historia();
-        historia.buildArvoreHistoria(capitulos);
+        historia.recuperarHistoria();
+        historia.salvarHistoria();
         historia.executar();
-        // salvarHistoria(historia);
     }
 
 
--- src/Historia.java
+++ src/Historia.java
@@ -13,78 +13,49 @@ public class Historia{
     private Personagem personagem = new Personagem();
     private Capitulo inicio;
     private State estado;
-    private Gson gson = new Gson();
 
     public void executar() throws InterruptedException, IOException{
-        restaurarEstadoO();
         System.out.print("\033[H\033[2J");
+        restaurarEstado();
         System.out.println("saude personagem: " + this.personagem.getSaude() + "\n");
         Capitulo aux = this.inicio;
         escolher(aux);
         
     }
 
-    private void restaurarEstadoO() throws FileNotFoundException {
-        File estado = new File("src/assets/estado.json");
-        if(estado.exists()){
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Desejá continuar de onde parou? \n 1 = s, 0 = n: ");
-            int opcao = scanner.nextInt();
-            System.out.println(opcao);
-            if(opcao == 1){
-                FileReader arquivo;
-                arquivo = new FileReader(estado);
-                JsonReader json = new JsonReader(arquivo);
-                this.estado = this.gson.fromJson(json, State.class);
-                this.inicio = this.estado.getCapituloAtual();
-                this.personagem.setSaude(this.estado.getSaudeAtual());
-                this.personagem.recuperaSaude();
-                this.personagem.setNome(this.estado.getNome());
-            }else{
-                estado.delete();
-            }
-        }
-    }
-
     private void escolher(Capitulo capitulo) throws InterruptedException, IOException {
         Scanner scanner = new Scanner(System.in);
-        System.out.println(capitulo.getTitulo());
-        System.out.println(" ");
-        System.out.println(capitulo.getContexto());
-        System.out.println(" ");
+        System.out.println(capitulo.getTitulo() + '\n');
+        System.out.println(capitulo.getContexto() + '\n');
 
         if(capitulo.getperdeperdeSaude() == true){
             this.personagem.perdeSaude();
         }
+
         System.out.println("saude personagem: " + this.personagem.getSaude() + "\n");
         if(capitulo.getMorre() == true){
+            scanner.close();
             return ;
         }
+
         if(this.personagem.getSaude() <= 0){
             System.out.println("saude personagem: " + this.personagem.getSaude() + "\n");
+            scanner.close();
             return ;
         }
 
-        System.out.println(capitulo.getEscolha01().getTitulo() + "\n");
-        System.out.println(capitulo.getEscolha02().getTitulo() + "\n");
-
-        System.out.println("Digite sua opção ou 0 para sair e salvar");
+        System.out.println(
+            capitulo.getEscolha01().getTitulo() + "\n" + 
+            capitulo.getEscolha02().getTitulo() + "\n" + 
+            "Digite sua opção ou 0 para sair e salvar"
+            );
 
         int escolha = scanner.nextInt();
 
         if(escolha == 0){
-            File arquivo = new File("/home/david/Documentos/workspace/historia-interativa-arvore-decisao/src/assets/estado.json");
-            if(!arquivo.exists()){
-                arquivo.createNewFile();
-            }
-            this.estado = new State();
-            this.estado.setSaudeAtual(this.personagem.getSaude());
-            this.estado.setNome(this.personagem.getNome());
-            this.estado.setCapituloAtual(capitulo);
-            FileWriter fileWriter = new FileWriter(arquivo);
-            this.gson.toJson(this.estado, fileWriter);
-            fileWriter.close();
-            return ;
+            salvarEstado(capitulo);
+            scanner.close();
+            return;
         }
         
         if (escolha == 1) {
@@ -109,25 +80,98 @@ public class Historia{
         scanner.close();
     }
 
+    public void salvarHistoria(){
+        try {
+            Gson gson = new Gson();
+            File arquivo = new File("src/assets/historia.json");
+            if(!arquivo.exists()){
+                arquivo.createNewFile();
+            }
+            FileWriter fileWriter = new FileWriter(arquivo);
+            Historia aux = this;
+            String json = gson.toJson(aux);
+            fileWriter.write(json);
+            fileWriter.close();
+            return ;
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    public void recuperarHistoria() throws IOException{
+        File historia = new File("src/assets/historia.json");
+        if(!historia.exists()){
+            historia.createNewFile();
+        }
+        FileReader historiaJson = new FileReader(historia);
+        Gson gson = new Gson();
+        JsonReader jsonRD = new JsonReader(historiaJson);
+        Historia aux = gson.fromJson(jsonRD, Historia.class);
+        this.estado = aux.estado;
+        this.inicio = aux.inicio;
+        this.personagem = aux.personagem;
+    }
+    
+    private void salvarEstado(Capitulo capitulo) throws IOException {
+        Gson gson = new Gson();
+        File arquivo = new File("src/assets/estado.json");
+        if(!arquivo.exists()){
+            arquivo.createNewFile();
+        }
+        this.estado = new State();
+        this.estado.setSaudeAtual(this.personagem.getSaude());
+        this.estado.setNome(this.personagem.getNome());
+        this.estado.setCapituloAtual(capitulo);
+        FileWriter fileWriter = new FileWriter(arquivo);
+        String json = gson.toJson(this.estado);
+        fileWriter.write(json);
+        fileWriter.close();
+    }
+
+    private void restaurarEstado() throws FileNotFoundException {
+        File estado = new File("src/assets/estado.json");
+        if(estado.exists()){
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Progresso encontrado: Desejá continuar de onde parou? \n S/n: ");
+            String opcao = scanner.nextLine();
+            System.out.println(opcao);
+            if(opcao == "S" || opcao == "s"){
+                FileReader arquivo;
+                arquivo = new FileReader(estado);
+                JsonReader json = new JsonReader(arquivo);
+                Gson gson = new Gson();
+                this.estado = gson.fromJson(json, State.class);
+                this.inicio = this.estado.getCapituloAtual();
+                this.personagem.setSaude(this.estado.getSaudeAtual());
+                this.personagem.recuperaSaude();
+                this.personagem.setNome(this.estado.getNome());
+            }else{
+                estado.delete();
+            }
+        }
+    }
+
     public void setInicio(Capitulo inicio) {
         this.inicio = inicio;
     }
 
     public void buildArvoreHistoria(JsonArray a){
+        Gson gson = new Gson();
         if(this.inicio == null){
             String json = a.get(0).toString();
-            Capitulo aux = this.gson.fromJson(json, Capitulo.class);
+            Capitulo aux = gson.fromJson(json, Capitulo.class);
             this.inicio = aux;
         }
         buildArvore(a);
     }
 
     private void buildArvore(JsonArray a) {
+        Gson gson = new Gson();
         Capitulo inicio = this.inicio;
         String _escolha01 = a.get(1).toString();
         String _escolha02 = a.get(2).toString();
-        Capitulo escolha01 = this.gson.fromJson(_escolha01, Capitulo.class);
-        Capitulo escolha02 = this.gson.fromJson(_escolha02, Capitulo.class);
+        Capitulo escolha01 = gson.fromJson(_escolha01, Capitulo.class);
+        Capitulo escolha02 = gson.fromJson(_escolha02, Capitulo.class);
 
         inicio.setEscolha01(escolha01);
         inicio.setEscolha02(escolha02);
@@ -137,14 +181,17 @@ public class Historia{
 
     private void buildArvore(Capitulo pai, JsonArray a, int i) {
         if(i < a.size()/2){
+            Gson gson = new Gson();
             String _escolha01 = a.get(i*2+1).toString();
             String _escolha02 = a.get(i*2 + 2).toString();
-            Capitulo escolha01 = this.gson.fromJson(_escolha01, Capitulo.class);
-            Capitulo escolha02 = this.gson.fromJson(_escolha02, Capitulo.class);
+            Capitulo escolha01 = gson.fromJson(_escolha01, Capitulo.class);
+            Capitulo escolha02 = gson.fromJson(_escolha02, Capitulo.class);
             pai.setEscolha01(escolha01);
             pai.setEscolha02(escolha02);
             buildArvore(pai.getEscolha01(), a, i+1);
             buildArvore(pai.getEscolha02(), a, i+2);
         }
     }
+
+    
 }
